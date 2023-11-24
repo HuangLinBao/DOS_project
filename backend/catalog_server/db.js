185bd6cd233824cc96fe3e53722a447e00546506
Tariq Sabri <57255961+HuangLinBao@users.noreply.github.com>
2023-11-24 15:58:18 +0200

    docker is working

--- backend/catalog_server/db.js
+++ backend/catalog_server/db.js
@@ -2,6 +2,6 @@ import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize("libDB", "user", "pass", {
   dialect: "sqlite",
-  host: "../database/books.sqlite",
+  host: "./database/books.sqlite",
 });
 export default sequelize;
